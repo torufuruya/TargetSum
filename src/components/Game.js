689ba0a08c5d4664575fccd3a82b30153bd03b63
Toru <t.furuya825@gmail.com>
2021-03-18 15:25:33 +0900

    Create Game UI

---
+++ src/components/Game.js
@@ -0,0 +1,56 @@
+ import React from 'react';
+ import { View, Text, StyleSheet } from 'react-native';
+
+ class Game extends React.Component {
+   randomNumbers = Array
+     .from({ length: this.props.randomNumberCount })
+     .map(() => 1 + Math.floor(10 * Math.random()))
+   target = this.randomNumbers
+     .slice(0, this.props.randomNumberCount - 2)
+     .reduce((acc, curr) => acc + curr, 0)
+   // TODO: Shuffle the random numbers
+   render() {
+     return (
+       <View style={styles.container}>
+         <Text style={styles.target}>{this.target}</Text>
+         <View style={styles.randomContainer}>
+           {this.randomNumbers.map((randomNumber, index) =>
+             <Text style={styles.random} key={index}>{randomNumber}</Text>
+           )}
+         </View>
+       </View>
+     );
+   };
+ };
+
+ const styles = StyleSheet.create({
+   container: {
+     backgroundColor: '#ddd',
+     flex: 1,
+   },
+
+   target: {
+     fontSize: 50,
+     backgroundColor: '#bbb',
+     margin: 50,
+     textAlign: 'center',
+   },
+
+   randomContainer: {
+     flex: 1,
+     flexDirection: 'row',
+     flexWrap: 'wrap',
+     justifyContent: 'space-around',
+   },
+
+   random: {
+     backgroundColor: '#999',
+     width: 100,
+     marginHorizontal: 15,
+     marginVertical: 25,
+     fontSize: 35,
+     textAlign: 'center',
+   },
+ });
+
+ export default Game;
