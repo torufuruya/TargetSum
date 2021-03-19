9d03f10cbc71a1333dbaf383b040cfd83c600eb7
Toru <t.furuya825@gmail.com>
2021-03-19 13:36:59 +0900

    Computing dynamic values where needed

--- src/components/Game.js
+++ src/components/Game.js
@@ -5,25 +5,41 @@ import RandomNumber from './RandomNumber';
 
 class Game extends React.Component {
   state = {
-    selectedNumbers: [],
+    selectedIds: [],
   };
   randomNumbers = Array
     .from({ length: this.props.randomNumberCount })
     .map(() => 1 + Math.floor(10 * Math.random()))
   target = this.randomNumbers
     .slice(0, this.props.randomNumberCount - 2)
-    .reduce((acc, curr) => acc + curr, 0)
+    .reduce((acc, curr) => acc + curr, 0);
   // TODO: Shuffle the random numbers
 
   isNumberSelected = (numberIndex) => {
-    return this .state.selectedNumbers.indexOf(numberIndex) >= 0;
+    return this .state.selectedIds.indexOf(numberIndex) >= 0;
   };
-  selecedNumbers = (numberIndex) => {
+  selectNumber = (numberIndex) => {
     this.setState((prevState) => ({
-      selectedNumbers: [...prevState.selectedNumbers, numberIndex],
+      selectedIds: [...prevState.selectedIds, numberIndex],
     }));
   };
+  // gameStatus: PLAYING, WON, LOST
+  gameStatus = () => {
+    const sumSelected = this.state.selectedIds.reduce((acc, curr) => {
+      return acc + this.randomNumbers[curr];
+    }, 0);
+    if (sumSelected < this.target) {
+      return 'PLAYING';
+    }
+    if (sumSelected === this.target) {
+      return 'WON';
+    }
+    if (sumSelected > this.target) {
+      return 'LOST';
+    }
+  };
   render() {
+    const gameStatus = this.gameStatus();
     return (
       <View style={styles.container}>
         <Text style={styles.target}>{this.target}</Text>
@@ -34,10 +50,11 @@ class Game extends React.Component {
               id={index}
               number={randomNumber}
               isDisabled={this.isNumberSelected(index)}
-              onPress={this.selecedNumbers}
+              onPress={this.selectNumber}
             />
           )}
         </View>
+        <Text>{gameStatus}</Text>
       </View>
     );
   };
