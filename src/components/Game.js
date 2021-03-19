90d37c1c55126924b500757e4b32051f5591e0b7
Toru <t.furuya825@gmail.com>
2021-03-19 14:02:38 +0900

    Working with timers

--- src/components/Game.js
+++ src/components/Game.js
@@ -6,6 +6,7 @@ import RandomNumber from './RandomNumber';
 class Game extends React.Component {
   state = {
     selectedIds: [],
+    remainingSeconds: this.props.initialSeconds,
   };
   randomNumbers = Array
     .from({ length: this.props.randomNumberCount })
@@ -14,6 +15,22 @@ class Game extends React.Component {
     .slice(0, this.props.randomNumberCount - 2)
     .reduce((acc, curr) => acc + curr, 0);
   // TODO: Shuffle the random numbers
+
+  componentDidMount() {
+    this.intervalId = setInterval(() => {
+      this.setState((prevState) => {
+        return { remainingSeconds: prevState.remainingSeconds - 1 };
+      }, () => {
+        if (this.state.remainingSeconds === 0) {
+          clearInterval(this.intervalId);
+        }
+      });
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.intervalId);
+  }
 
   isNumberSelected = (numberIndex) => {
     return this .state.selectedIds.indexOf(numberIndex) >= 0;
@@ -28,6 +45,9 @@ class Game extends React.Component {
     const sumSelected = this.state.selectedIds.reduce((acc, curr) => {
       return acc + this.randomNumbers[curr];
     }, 0);
+    if (this.state.remainingSeconds === 0) {
+      return 'LOST';
+    }
     if (sumSelected < this.target) {
       return 'PLAYING';
     }
@@ -54,7 +74,7 @@ class Game extends React.Component {
             />
           )}
         </View>
-        <Text>{gameStatus}</Text>
+        <Text>{this.state.remainingSeconds}</Text>
       </View>
     );
   };
